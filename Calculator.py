1c1dcd0df196ebfebf8a4c3a4636fb2b6eef03d3
Evilgirl7005 <166894143+Evilgirl7005@users.noreply.github.com>
2024-04-22 11:27:06 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+class Calculator():
+    def __init__(self):
+        pass
+    def addition(self):
+        a = int(input("Enter the 1st number"))
+        b= int(input("Enter the 2nd number"))
+        return a+b
+    def subtraction(self):
+        a = int(input("Enter the 1st number"))
+        b= int(input("Enter the 2nd number"))
+        
+        return a-b 
+    def Multiplication(self):
+        a = int(input("Enter the 1st number"))
+        b= int(input("Enter the 2nd number"))
+        
+        return a*b
+    def division(self):
+        a = int(input("Enter the 1st number"))
+        b= int(input("Enter the 2nd number"))
+       
+        return int(a/b)
+
+c = Calculator()
